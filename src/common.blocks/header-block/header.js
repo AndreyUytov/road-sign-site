ef61e81b856f3594fcdb871fdaedf643b7e5f13a
Андрей <uytov1991@gmail.com>
2021-02-20 19:30:54 +0300

    доделать всплытие dropdown

--- src/common.blocks/header-block/header.js
+++ src/common.blocks/header-block/header.js
@@ -1,5 +1,14 @@
-const menuToggle = document.querySelector('.page-header__menu-toggle')
+const menuToggle = document.querySelector('.page-header__menu-toggle');
+const submenuToggle = document.querySelector('.page-header__nav-item--submenu');
 
 menuToggle.addEventListener('click', () => {
-  this.classList.toggle('page-header__menu-toggle--show')
-})
+  menuToggle.classList.toggle('page-header__menu-toggle--show');
+
+  if (menuToggle.classList.contains('page-header__menu-toggle--show')) {
+    document.body.style.overflow = 'hidden';
+  } else document.body.style.overflow = '';
+});
+
+submenuToggle.addEventListener('click', () => {
+  submenuToggle.classList.toggle('submenu-show');
+});
