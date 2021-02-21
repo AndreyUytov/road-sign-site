55686692870b788621be8ecbad7489ebd91df801
Андрей <uytov1991@gmail.com>
2021-02-21 20:07:12 +0300

    закончил стилизацию smart-search

--- src/vue/components/smart-search.vue
+++ src/vue/components/smart-search.vue
@@ -1,27 +1,32 @@
 <template>
-  <div class="smart-search__wrapper">
-    <button class="smart-search__show-snap">
+  <div class="smart-search__wrapper"
+  :class="{'smart-search__wrapper--hide': !isOpen}">
+    <button @click="smartSearchToggle" 
+    v-show="!isOpen"
+     class="smart-search__show-snap">
       <svg class="smart-search__show-snap-svg">
         <use href="#search" />
       </svg>
     </button>
-    <div class="smart-search">
-      <label class="smart-search__label">
-        <input type="text" placeholder="Поиск" class="smart-search__input"/>
+    <div class="smart-search" v-show="isOpen">
+      <div class="label-and-result-wrapper">
+        <label class="smart-search__label">
+        <input type="text" placeholder="Поиск" class="smart-search__input" v-model="inputValue" ref="input"/>
         <button class="smart-search__submit">
           <svg class="smart-search__submit-svg">
             <use href="#search" />
           </svg>
         </button>
-        <ul class="smart-search__result-list">
+      </label>
+      <ul class="smart-search__result-list">
           <li class="result-list__item">
             <a href="#" class="result-list__link">
               Дорожные знаки
             </a>
           </li>
         </ul>
-      </label>
-      <button class="smart-search__close-snap">
+      </div>
+      <button @click="smartSearchToggle" class="smart-search__close-snap">
         <svg class="smart-search__close-snap-svg">
           <use href="#close" />
         </svg>
@@ -33,28 +38,58 @@
 
 <script>
 export default {
-  name: 'SmartSearch'
+  name: 'SmartSearch',
+  data() {
+    return {
+      inputValue: '',
+      isOpen: false
+    }
+  },
+  methods: {
+    smartSearchToggle() {
+      this.isOpen = !this.isOpen
+      this.isOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = ''
+
+      this.isOpen ? this.$refs.input.focus() : null
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
   .smart-search__wrapper {
-    position: absolute;
-    top: 10px;
+    position: fixed;
+    top: 0px;
     left: 0px;
 
     width: 100%;
-    height: 40px;
+    height: 56px;
+    padding: 0 16px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
 
     background: #2B2831;
   }
 
-  .smart-search__show-snap {
-    widows: 24px;
+    .smart-search__wrapper--hide {
+    position: static;
+    width: 24px;
     height: 24px;
+    padding: 0;
 
-    display: none;
+    overflow: hidden;
+    filter: none;
+  }
+
+  .smart-search__show-snap {
+    width: 24px;
+    height: 24px;
+    padding: 0;
+
+    display: flex;
     justify-content: center;
     align-items: center;
 
@@ -74,17 +109,22 @@ export default {
   }
 
   .smart-search {
+    position: relative;
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
+  .label-and-result-wrapper {
+    width: 100%;
+    margin-right: 24px;
+  }
+
   .smart-search__label {
     position: relative;
-    width: 244px;
+    width: 100%;
     height: 100%;
-    margin-right: 24px;
 
     display: flex;
   }
@@ -92,6 +132,19 @@ export default {
   .smart-search__input {
     width: 100%;
     padding: 8px 48px 8px 16px;
+
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #fff;
+
+    background: transparent;
+    border: 1px solid #858585;
+    border-radius: 20px;
+
+    outline: none;
   }
 
   .smart-search__submit {
@@ -119,7 +172,31 @@ export default {
   }
 
   .smart-search__result-list {
-    display: none;
+    position: fixed;
+    top: 56px;
+    left: 0px;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    padding-top: 24px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    list-style: none;
+
+    background: #2B2831;
+  }
+
+  .result-list__item {
+    width: 100%;
+    padding: 8px 16px;
+
+    &:hover {
+      background: #3A3A47;
+    }
   }
 
   .smart-search__close-snap {
@@ -141,5 +218,32 @@ export default {
     height: 24px;
 
     --svg-fill: #858585;
+  }
+
+  @media(min-width:1550px) {
+    .smart-search__wrapper {
+      height: 72px;
+      filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    }
+
+    .smart-search__wrapper--hide {
+      filter: none;
+    }
+
+    .smart-search {
+      width: 1450px;
+      padding: 0 16px;
+      margin: 0 auto;
+    }
+
+    .label-and-result-wrapper {
+      position: relative;
+    }
+    .smart-search__result-list {
+      position: absolute;
+      padding-top: 4px;
+
+      background-clip: content-box;
+    }
   }
 </style>
